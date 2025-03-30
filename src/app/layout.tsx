275cc3773cb5020453730cd1c579b307920c1034
Matheus Peviani Vellone <matheusvellone@gmail.com>
2025-03-30 16:33:07 -0300

    disable mobile zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Viewport } from 'next'
 import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { PropsWithChildren } from 'react'
@@ -30,3 +31,22 @@ const RootLayout = ({ children }: PropsWithChildren) => {
 }
 
 export default trpc.withTRPC(RootLayout)
+
+export const viewport: Viewport = {
+  themeColor: [
+    {
+      color: '#FFF',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      color: '#242424',
+      media: '(prefers-color-scheme: dark)',
+    },
+  ],
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  width: 'device-width',
+  userScalable: false,
+  viewportFit: 'cover',
+}
